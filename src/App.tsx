e93290170d61d1bbf9e3171fcdd549e769681004
eduardonk9999 <thiago.novak7@gmail.com>
2022-08-16 14:28:35 -0300

    final projeto

--- src/App.tsx
+++ src/App.tsx
@@ -6,28 +6,22 @@ function App() {
   const posts = [
     {
       id: "0",
-      content:'Oi sou um post',
-      userName:'Eduardo Silva',
-      avatarUrl:'https://avatars.githubusercontent.com/u/18013936?s=400&u=e4a393ee9e0d662a666364083458f63dc0ac4489&v=4'
-    },
-    {
-      id: "1",
       userName: "Anthony Carvalho",
-      avatar: "https://cdn.fakercloud.com/avatars/tobysaxon_128.jpg",
+      avatar: "https://avatars.githubusercontent.com/u/18013936?s=400&u=e4a393ee9e0d662a666364083458f63dc0ac4489&v=4",
       content:
         "Aut amet magnam. Exercitationem et cupiditate unde voluptas est et. Est explicabo repellat voluptas dicta qui. Laboriosam sapiente quasi maxime omnis accusantium quo.",
     },
     {
-      id: "2",
+      id: "1",
       userName: "Yago Franco Neto",
-      avatar: "https://cdn.fakercloud.com/avatars/strikewan_128.jpg",
+      avatar: "https://avatars.githubusercontent.com/u/18013936?s=400&u=e4a393ee9e0d662a666364083458f63dc0ac4489&v=4",
       content:
         "Atque ipsam maxime blanditiis dolor ducimus. Adipisci quis occaecati natus qui saepe. Est ea porro est nisi magni voluptate consequatur sint reprehenderit. Quas aut quae et ex sed illum. Minima magni fugit omnis vel rerum distinctio. Voluptas consequatur corporis natus mollitia est eos neque et exercitationem.",
     },
     {
-      id: "3",
+      id: "2",
       userName: "Yago Franco Neto",
-      avatar: "https://cdn.fakercloud.com/avatars/strikewan_128.jpg",
+      avatar: "https://avatars.githubusercontent.com/u/18013936?s=400&u=e4a393ee9e0d662a666364083458f63dc0ac4489&v=4",
       content:
         "Atque ipsam maxime blanditiis dolor ducimus. Adipisci quis occaecati natus qui saepe. Est ea porro est nisi magni voluptate consequatur sint reprehenderit. Quas aut quae et ex sed illum. Minima magni fugit omnis vel rerum distinctio. Voluptas consequatur corporis natus mollitia est eos neque et exercitationem.",
     },
@@ -35,11 +29,15 @@ function App() {
 
   return (
    <>
+   {posts.map((post, index) => (
     <Post
-      content='Oi sou um post'
-      userName='Eduardo Silva'
-      avatarUrl='https://avatars.githubusercontent.com/u/18013936?s=400&u=e4a393ee9e0d662a666364083458f63dc0ac4489&v=4'
+      key={index}
+      content={post.content}
+      userName={post.userName}
+      avatarUrl={post.avatar}
     />
+   ))}
+    
    </>
   )
 }
